b7c03d3527ffdc0029e30d01508cf206a6c10436
SirDennis <lilyngure@gmail.com>
2024-11-07 14:17:46 +0300

    sdk set up update

--- sample/android/src/main/java/com/dennis/multicontacts/MainActivity.kt
+++ sample/android/src/main/java/com/dennis/multicontacts/MainActivity.kt
@@ -3,7 +3,6 @@ package com.dennis.multicontacts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
